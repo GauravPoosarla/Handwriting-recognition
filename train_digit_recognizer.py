408e0eb0aa043db04ec2ace2bec394906fe461a1
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2020-05-30 17:09:07 +0530

    Increased the readability.

--- train_digit_recognizer.py
+++ train_digit_recognizer.py
@@ -1,3 +1,4 @@
+#importing libraries.
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
@@ -7,9 +8,11 @@ from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import Flatten
 from keras.losses import categorical_crossentropy
+
 # the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+#reshaping the image in the form it is there.
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 input_shape = (28, 28, 1)
@@ -25,16 +28,25 @@ x_test /= 255
 
 #Building of CNN.
 model = Sequential()
+#Adding the first convoluted layer with a 5 x 5 feature detector.
 model.add(Conv2D(32, kernel_size=(5, 5),activation='relu',input_shape=input_shape))
+#Adding a pooling layer with a kernel of 3 x 3.
 model.add(MaxPooling2D(pool_size=(3, 3)))
+#Adding the second convoluted layer and pooling layer.
 model.add(Conv2D(64, (4, 4), activation='relu'))
 model.add(MaxPooling2D(pool_size=(3, 3)))
+#Making of the fully connected layer.
 model.add(Flatten())
+#Adding the first hidden layer.
 model.add(Dense(128, activation='relu'))
+#Adding a dropout of 0.3, to prevent overfitting.
 model.add(Dropout(0.3))
+#Adding second hidden layer.
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
+#Using softmax function because we need probabilistic output for each class.
 model.add(Dense(num_classes, activation='softmax'))
+#compiling of the model.
 model.compile(loss=categorical_crossentropy,optimizer="adam",metrics=['accuracy'])
 
 #Saving of the model.
